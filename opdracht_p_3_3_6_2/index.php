1f0d2c20d1ca72ace6727eb6b4347041089d53ba
Rouhi1432 <112872104+Rouhi1432@users.noreply.github.com>
2023-04-19 10:44:56 +0200

    19_4_

--- opdracht_p_3_3_6_2/index.php
+++ opdracht_p_3_3_6_2/index.php
@@ -1,28 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="nl">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>opdrachtP3_3-62</title>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>jens rakan</title>
 </head>
-
 <body>
-    <?php
-    session_start();
-    $counter = 0;
-    $_SESSION["counter"] += 1;
-    echo "Deze pagina heb je al " . $_SESSION["counter"] . " keer bekeken voordat je de internet browser hebt afgesloten.";
-    if (!isset($_COOKIE['cookietime'])) {
-        setcookie('cookietime', $_SESSION['sessiontime'], time() + 3600);
-    }
-    if ($_COOKIE['cookietime'] <= $_SESSION['sessiontime']) {
-        $_COOKIE['cookietime'] = $_SESSION['sessiontime'];
-    }
-    echo 'U heeft de pagina in
-     totaal ' . $_COOKIE['cookietime'] . ' keer bezocht.';
-    ?>
+ <form action="#" method="post">
+ <label for="age">Leeftijd:</label>
+<input id='number' type="number" name="number"> <br>
+ <label for="stemvrg">Heeft u een stempas?</label><br>
+ <input id='stempasja' type="radio" name="stempasja">ja</input><br>
+ <input id='stempasnee' type="radio" name="stempasnee">nee</input><br>
+ <input type="submit" name="verzend">
+</form>
+ <?php
+if (isset($_POST['verzend'])) {
+$age = $_POST['number'];
+ if ($age >= 16) {
+ echo "Je mag rijexamen voor je scooterrijbewijs doen.";
+ } elseif ($age < 16) {
+ echo "Je bent te jong om uw scooterrijbewijs te halen";
+}
+ };
+ if (isset($_POST['stempasja']) && $age >= 18) {
+ echo "<br> Je mag stemmen";
+ }
+ elseif (isset($_POST['stempasnee']) && $age <= 17) {
+ echo "<br> Je mag niet stemmen";
+ }
+ elseif (isset($_POST['stempasja']) && $age <= 17) {
+ echo "<br> Je mag niet stemmen";
+ }
+?>
+
 </body>
+
+
+
 
 </html>
