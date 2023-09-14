0fcc726bf94004524287c9563d548e856a0d046a
Rouhi1432 <112872104+Rouhi1432@users.noreply.github.com>
2023-09-14 14:42:47 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,593 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+</head>
+<body>
+<section class="container">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+
+      <span class="navbar-toggler-icon"></span>
+
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<form>
+
+  <div class="mb-3">
+
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+  </div>
+
+  <div class="mb-3">
+
+    <label for="exampleInputPassword1" class="form-label">Leeftijd</label>
+
+    <input type="number" class="form-control" id="exampleInputPassword1">
+
+  </div>
+
+  <div class="mb-3">
+
+    <label for="exampleInputPassword1" class="form-label">geboortejaar</label>
+
+    <input type="date" class="form-control" id="exampleInputPassword1">
+
+  </div>
+
+  <div class="mb-3">
+
+    <label for="exampleInputPassword1" class="form-label">land</label>
+
+    <input type="text" class="form-control" id="exampleInputPassword1">
+
+  </div>
+
+  <div class="mb-3">
+
+    <label for="exampleInputPassword1" class="form-label">telefoonNummer</label>
+
+    <input type="number" class="form-control" id="exampleInputPassword1">
+
+  </div>
+
+  <div class="mb-3">
+
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+
+    <input type="password" class="form-control" id="exampleInputPassword1">
+
+  </div>
+
+  <div class="mb-3 form-check">
+
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
+<br>
+      <div class="row row-cols-1 row-cols-md-4 g-4">
+
+        <div class="col">
+
+          <div class="card">
+
+            <img src="..." class="card-img-top" alt="...">
+
+            <div class="card-body">
+
+              <h5 class="card-title">Card title</h5>
+
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="col">
+
+          <div class="card">
+
+            <img src="..." class="card-img-top" alt="...">
+
+            <div class="card-body">
+
+              <h5 class="card-title">Card title</h5>
+
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="col">
+
+          <div class="card">
+
+            <img src="..." class="card-img-top" alt="...">
+
+            <div class="card-body">
+
+              <h5 class="card-title">Card title</h5>
+
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="col">
+
+          <div class="card">
+
+            <img src="..." class="card-img-top" alt="...">
+
+            <div class="card-body">
+
+              <h5 class="card-title">Card title</h5>
+
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="row row-cols-1 row-cols-md-4 g-4">
+
+        <div class="col">
+
+          <div class="card">
+
+            <img src="..." class="card-img-top" alt="...">
+
+            <div class="card-body">
+
+              <h5 class="card-title">Card title</h5>
+
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="col">
+
+          <div class="card">
+
+            <img src="..." class="card-img-top" alt="...">
+
+            <div class="card-body">
+
+              <h5 class="card-title">Card title</h5>
+
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="col">
+
+          <div class="card">
+
+            <img src="..." class="card-img-top" alt="...">
+
+            <div class="card-body">
+
+              <h5 class="card-title">Card title</h5>
+
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="col">
+
+          <div class="card">
+
+            <img src="..." class="card-img-top" alt="...">
+
+            <div class="card-body">
+
+              <h5 class="card-title">Card title</h5>
+
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <table class="table">
+
+        <thead>
+
+          <tr>
+
+            <th scope="col">#</th>
+
+            <th scope="col">Letters</th>
+
+            <th scope="col">Nummers</th>
+
+            <th scope="col">Symbolen</th>
+
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          <tr>
+
+            <th scope="row">1</th>
+
+            <td>A</td>
+
+            <td>1</td>
+
+            <td>!</td>
+
+          </tr>
+
+          <tr>
+
+            <th scope="row">2</th>
+
+            <td>B</td>
+
+            <td>2</td>
+
+            <td>@</td>
+
+          </tr>
+
+          <tr>
+
+            <th scope="row">3</th>
+
+            <td>C</td>
+
+            <td>3</td>
+
+            <td>#</td>
+
+          </tr>
+
+          <tr>
+
+            <th scope="row">4</th>
+
+            <td>D</td>
+
+            <td>4</td>
+
+            <td>$</td>
+
+          </tr>
+
+          <tr>
+
+            <th scope="row">5</th>
+
+            <td>E</td>
+
+            <td>5</td>
+
+            <td>%</td>
+
+          </tr>
+
+        </tbody>
+
+      </table>
+
+      <center>
+
+      <nav aria-label="Page navigation example">
+
+        <ul class="pagination">
+
+          <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+
+          <li class="page-item"><a class="page-link" href="#">Next</a></li>
+
+        </ul>
+
+      </nav>
+    </center>
+<footer class="text-center text-lg-start bg-white text-muted">
+
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+
+
+    <div class="me-5 d-none d-lg-block">
+
+      <span>Get connected with us on social networks:</span>
+
+    </div>
+    <div>
+
+      <a href="" class="me-4 link-secondary">
+
+        <i class="fab fa-facebook-f"></i>
+
+      </a>
+
+      <a href="" class="me-4 link-secondary">
+
+        <i class="fab fa-twitter"></i>
+
+      </a>
+
+      <a href="" class="me-4 link-secondary">
+
+        <i class="fab fa-google"></i>
+
+      </a>
+
+      <a href="" class="me-4 link-secondary">
+
+        <i class="fab fa-instagram"></i>
+
+      </a>
+
+      <a href="" class="me-4 link-secondary">
+
+        <i class="fab fa-linkedin"></i>
+
+      </a>
+
+      <a href="" class="me-4 link-secondary">
+
+        <i class="fab fa-github"></i>
+
+      </a>
+
+    </div>
+
+   
+
+  </section>
+
+  <section class="">
+
+    <div class="container text-center text-md-start mt-5">
+
+
+      <div class="row mt-3">
+
+
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+         
+
+          <h6 class="text-uppercase fw-bold mb-4">
+
+            <i class="fas fa-gem me-3 text-secondary"></i>Company name
+
+          </h6>
+
+          <p>
+
+            Here you can use rows and columns to organize your footer content. Lorem ipsum
+
+            dolor sit amet, consectetur adipisicing elit.
+
+          </p>
+
+        </div>
+
+       
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+     
+
+          <h6 class="text-uppercase fw-bold mb-4">
+
+            Products
+
+          </h6>
+
+          <p>
+
+            <a href="#!" class="text-reset">Angular</a>
+
+          </p>
+
+          <p>
+
+            <a href="#!" class="text-reset">React</a>
+
+          </p>
+
+          <p>
+
+            <a href="#!" class="text-reset">Vue</a>
+
+          </p>
+
+          <p>
+
+            <a href="#!" class="text-reset">Laravel</a>
+
+          </p>
+
+        </div>
+
+       
+
+ 
+
+     
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+       
+
+          <h6 class="text-uppercase fw-bold mb-4">
+
+            Useful links
+
+          </h6>
+
+          <p>
+
+            <a href="#!" class="text-reset">Pricing</a>
+
+          </p>
+
+          <p>
+
+            <a href="#!" class="text-reset">Settings</a>
+
+          </p>
+
+          <p>
+
+            <a href="#!" class="text-reset">Orders</a>
+
+          </p>
+
+          <p>
+
+            <a href="#!" class="text-reset">Help</a>
+
+          </p>
+
+        </div>
+
+       
+
+ 
+
+       
+
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+       
+
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+
+          <p><i class="fas fa-home me-3 text-secondary"></i> New York, NY 10012, US</p>
+
+          <p>
+
+            <i class="fas fa-envelope me-3 text-secondary"></i>
+
+            info@example.com
+
+          </p>
+
+          <p><i class="fas fa-phone me-3 text-secondary"></i> + 01 234 567 88</p>
+
+          <p><i class="fas fa-print me-3 text-secondary"></i> + 01 234 567 89</p>
+
+        </div>
+
+       
+
+      </div>
+
+   
+
+    </div>
+
+  </section>
+
+ 
+
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+
+    © 2021 Copyright:
+
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+
+  </div>
+
+ 
+
+</footer>
+
+ 
+
+    </section>
+
+ 
+
+   
+
+   
+
+</body>
+
+</html>
