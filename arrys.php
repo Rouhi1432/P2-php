9dca467239779571ecad9e85651d4eb2d493055c
Rouhi1432 <112872104+Rouhi1432@users.noreply.github.com>
2023-09-19 15:24:57 +0200

    Add files via upload

---
+++ arrys.php
@@ -0,0 +1,13 @@
+<?php
+$brands = [ "Nike", "adidas", "Puma", "mcenzie"],
+foreach ($brands as $braand ){ 
+    ?>
+<<p>$brands</p>
+<?Php
+} ;
+<h1>Het derde merk: " . $brands[2] . "</h1>";
+
+
+
+
+?>
---
+++ arrys1.php
@@ -0,0 +1,29 @@
+
+<?php
+/*
+$languages = [
+    ["HTML", "CSS"],
+    ["Python", "Java"],
+    ["Unity", "C#"]
+];
+foreach ($languages[0] as $language ){ 
+    ?>
+<<p> $language zijn frontend talen< /p>
+    <?php
+
+};
+$language[0 , 1] "HTML en CSS zijn frontend talen";
+
+
+
+
+
+
+
+*/
+
+$persons = [ "John" => 30,"Alice" => 25,"Bob" => 35,"Eva" => 28, "Michael" => 40];
+foreach($persons as $name => $age){
+echo "mijn naam is" . $name . "en ik ben " . $age . "jaar oud" ;
+}
+?>
